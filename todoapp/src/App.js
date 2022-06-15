a6f0b261c003afce85573615dcf2db9b0b28de03
北村悠真 <washiasaride@gmail.com>
2022-06-15 10:09:06 +0900

    second commit

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -1,6 +1,12 @@
-console.log("App.js: loaded");
-export class App{
-    constructor(){
-        console.log("App initialized");
-    }
+export class App {
+  mount() {
+    const formElement = document.querySelector("#js-form");
+    const inputElement = document.querySelector("#js-form-input");
+    console.log(formElement);
+    console.log(inputElement);
+    formElement.addEventListener("submit", (event) => {
+      event.preventDefault();
+      console.log(`入力値の値:${inputElement.value}`);
+    });
+  }
 }
